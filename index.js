f3907495e1d2229378195d84bad873711f03e90c
KAYABINGHI <muchaipauls@gmail.com>
2025-06-14 13:59:42 +0300

    distanceFromHq

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(scooter) {
+  const hqLocation = 42;
+  const distance = Math.abs(scooter - hqLocation);
+  return distance;
+}
+function distanceFromHqInFeet(scooter) {
+  const blocks = distanceFromHqInBlocks(scooter);
+  const feetPerBlock = 264;
+  return blocks * feetPerBlock;
+}
+function distanceTravelledInFeet(start, destination) {
+  const blocks = Math.abs(destination - start);
+  const feetPerBlock = 264;
+  return blocks * feetPerBlock;
+}
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02;
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
